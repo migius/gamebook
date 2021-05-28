a73ddabe1457337069bbad1299c766b1d33e6d95
Bruni Michele <michelebruni87@gmail.com>
2021-05-28 22:36:27 +0200

    aggiunta scheda carmilla

--- carmilla/res/i18n.js
+++ carmilla/res/i18n.js
@@ -1,6 +1,6 @@
 const messages = {
     it: {        
-        registro: "Registro Laura",
+        registro_laura: "Registro Laura",
         intuito: "intuito",
         salute: "salute",
         oggetti: "oggetti",
@@ -12,7 +12,9 @@ const messages = {
             benvenuto: "Benvenuto",
             new_game_confirm: "Sei sicuro di voler iniziare una nova partita? Si cancelleranno le statistiche correnti.",
             go_to_226: "Vai al 226.",
+            go_to_299: "Vai al 299.",
             salute_0: "Il tuo fisico ha ceduto e sei sconfitta. Vuoi iniziare una nuova partita resettando tutte le statistiche?",
+            rigenerazione_0: "I tuoi poteri svaniscono. Vuoi iniziare una nuova partita resettando tutte le statistiche?",
         },        
         url_amazon: `https://www.amazon.it/gp/product/8887224102/ref=as_li_tl?ie=UTF8&camp=3414&creative=21718&creativeASIN=8887224102&linkCode=as2&tag=migio21-21&linkId=65425fc1f405cdd96e7ff633ccb1b10c`,
         link_amazon: `<a target="_blank" href="@:url_amazon">Carmilla. Il bacio del vampiro</a>`,
@@ -25,14 +27,27 @@ const messages = {
             bg_foto: `Foto sullo sfondo: <a href='https://www.freepik.com/photos/tree'  target="_blak">Tree photo created by wirestock - www.freepik.com</a>`,
             send_feedback: `Per segnalare malfunzionamenti o suggerire modifiche <a href="https://migio.altervista.org/contatti/" target="popup" >Invia un feedback</a>`,
             howto: "Come funziona",
-            howto_content: `Questa scheda è di ausilio alla lettura del librogame @:link_amazon di Francesco Di Lazzaro edito da Watson Edizioni.<br>
+            howto_content_laura: `Questa scheda è di ausilio alla lettura del librogame @:link_amazon di Francesco Di Lazzaro edito da Watson Edizioni.<br>
             Per modificare i valori salute e intuito utilizzare i pulsanti - e +.<br>
             Per aggiungere oggetti cliccare il quadrato oggetti, per rimuoverli basta cancellarne il nome.<br>
             Per aggiungere e rimouvere parole chiave e note utilizzare i tasti + e meno -.<br>
             <br>
             I valori presenti nella scheda (e nessun altro dato) vengono salvati nel browser e quindi anche se la pagina viene chiusa e riaperta i dati restano salvati. Non è possibile scaricare o inviare i propri dati a un altro dispositivo. Nessun dato viene inviato a server centrali, la partita rimarrà solo sul dispositivo utilizzato.<br>
+            Se si vuole resettare i valori utilizzare il tasto '@:new_game'.`,
+            howto_content_carmilla: `Questa scheda è di ausilio alla lettura del librogame @:link_amazon di Francesco Di Lazzaro edito da Watson Edizioni.<br>
+            Per modificare i valori oscurità e rigenerazione utilizzare i pulsanti - e +.<br>
+            Per aggiungere oggetti cliccare il quadrato oggetti, per rimuoverli basta cancellarne il nome.<br>
+            Per segnare il sangue di Laura basta cliccare sui quadrati<br>
+            Per aggiungere e rimouvere parole chiave e note utilizzare i tasti + e meno -.<br>
+            <br>
+            I valori presenti nella scheda (e nessun altro dato) vengono salvati nel browser e quindi anche se la pagina viene chiusa e riaperta i dati restano salvati. Non è possibile scaricare o inviare i propri dati a un altro dispositivo. Nessun dato viene inviato a server centrali, la partita rimarrà solo sul dispositivo utilizzato.<br>
             Se si vuole resettare i valori utilizzare il tasto '@:new_game'.`
-        }
+        },
+        //carmilla
+        registro_carmilla: "Registro Carmilla",
+        oscurita: "oscurità",
+        rigenerazione: "rigenerazione",
+        sangue_laura: "Il sangue di Laura"
     },
     en: {  
         //todo
---
+++ carmilla/res/logic-carmilla.js
@@ -0,0 +1,211 @@
+//##########################
+//utilities
+//##########################
+
+function isEmptyOrSpaces(str){
+    return str === null || str.match(/^ *$/) !== null;
+}
+
+//##########################
+//objects
+//##########################
+
+function ScalaVampItem(name, customVal, number){
+    if(name !== "") {
+        this.name = name;
+    }else{
+        this.name = number;
+    }
+
+    this.customVal = customVal;
+    this.number = number;
+    
+    this.value = function(vamp){
+            if(this.number == vamp) {
+                return "X";
+            }
+            else
+                return " ";
+        if(this.customVal !== ""){
+        }
+        else
+            return this.customVal;
+    }
+}
+
+const statTime = (new Date()).getTime();
+
+//##########################
+// vue
+//##########################
+var main = new Vue({
+    i18n,
+    el: '#main',
+    data: {
+        //saved to localDB
+        gameStats: {
+            oscurita: 5,
+            rigenerazione: 4,
+            oggetti: [],
+            vampirizzazione: 5,
+            paroleChiave: [],
+            note: [],
+        },
+        //not saved
+        activeEdit: null,
+        maxOscurita: 5,
+        minOscurita: 0,
+        maxRigenerazione: 4,
+        minRigenerazione: 0,
+        maxVampirizzazione: 8,
+        minVampirizzazione: 0,
+        scalaVampirizzazione: [
+            new ScalaVampItem("", "vai al 299", 0),
+            new ScalaVampItem("", "", 1),
+            new ScalaVampItem("", "", 2),
+            new ScalaVampItem("", "", 3),
+            new ScalaVampItem("", "", 4),
+            new ScalaVampItem("", "", 5),
+            new ScalaVampItem("", "", 6),
+            new ScalaVampItem("", "", 7),
+            new ScalaVampItem("", "", 8),
+        ]
+    },
+    computed: {
+    },
+    methods: { 
+        loadDummy: function(){
+
+            //oggetti
+            if(this.gameStats.oggetti[0] === undefined) this.gameStats.oggetti[0].name = "coltello";
+            if(this.gameStats.oggetti[1] === undefined) this.gameStats.oggetti[1].name = "fiore";
+            if(this.gameStats.oggetti[2] === undefined) this.gameStats.oggetti[2].name = "spilla";
+            if(this.gameStats.oggetti[3] === undefined) this.gameStats.oggetti[3].name = "chiave";
+
+            //paroleChiave
+            this.gameStats.paroleChiave.push({name: "parola 1"});
+            this.gameStats.paroleChiave.push({name: "ciao 2"});
+            this.gameStats.paroleChiave.push({name: "parola 3"});
+
+            //note
+            this.gameStats.note.push({name: "nota 1 nota 1 nota 1 nota 1 nota 1 nota 1 nota 1 nota 1"});
+            this.gameStats.note.push({name: "nota 2 nota 2 nota 2 nota 2 nota 2 nota 2 nota 2"});
+            this.gameStats.note.push({name: "nota 3 nota 3 nota 3 nota 3 nota 3 nota 3 nota 3 nota 3 nota 3 nota 3 nota 3 nota 3 nota 3"});
+        },
+        addOscurita: function(value){
+            this.gameStats.oscurita = this.gameStats.oscurita + value;
+            this.saveToLocalDB();
+        },
+        addRigenerazione: function(value){
+            this.gameStats.rigenerazione = this.gameStats.rigenerazione + value;
+            this.saveToLocalDB();
+            Vue.nextTick(function() {
+                if(main.gameStats.rigenerazione === 0) 
+                    if(confirm(main.$t("messaggi.rigenerazione_0")))
+                        main.startNewGame(true);
+            });
+        },
+        addVampirizzazione: function(value){
+            if(this.gameStats.vampirizzazione + value > this.maxVampirizzazione ||
+                this.gameStats.vampirizzazione + value < this.minVampirizzazione)
+                return;
+            this.gameStats.vampirizzazione = this.gameStats.vampirizzazione + value;
+            this.saveToLocalDB();   
+            Vue.nextTick(function() {
+                if(main.gameStats.vampirizzazione === 0) alert(main.$t("messaggi.go_to_226"));
+            });
+        },
+        editItem: function(item){
+            this.activeEdit = item;
+            //esegue dopo refresh
+            Vue.nextTick(function() {
+                document.getElementsByClassName("active-input")[0].focus();
+            });
+            //event.preventDefault();
+        },
+        doneEditItem: function(){
+            if(this.activeEdit != undefined && isEmptyOrSpaces(this.activeEdit.name)) this.activeEdit.name = "\xa0";
+            this.activeEdit = null;
+            this.saveToLocalDB();
+        },
+        saveToLocalDB: function(){
+            localStorage.setItem('GB_carmilla_carmilla', JSON.stringify(this.gameStats));
+        },
+        loadFromLocalDB: function(){
+            let data = JSON.parse(localStorage.getItem('GB_carmilla_carmilla'));
+            if(data == null) {
+                this.startNewGame();
+            } else {
+                this.gameStats = data;
+            }
+        },
+        startNewGame: function(withoutConfirm = false){
+            if(withoutConfirm || confirm(this.$t("messaggi.new_game_confirm"))){
+                this.gameStats.oscurita = 5;
+                this.gameStats.rigenerazione = 4;
+                this.gameStats.oggetti = [{name: "\xa0"}, {name: "\xa0"}, {name: "\xa0"}, {name: "\xa0"}];
+                this.gameStats.vampirizzazione = 5;
+                this.gameStats.paroleChiave = [];
+                this.gameStats.note = [];
+                this.saveToLocalDB();
+            }
+        },
+        addParolaChiave: function(){
+            let s = {name: ""};
+            this.gameStats.paroleChiave.push(s);
+            this.editItem(s);
+        },
+        addNota: function(){
+            let s = {name: ""};
+            this.gameStats.note.push(s);            
+            this.editItem(s);
+        },
+        removeParolaChiave: function(item){
+            this.gameStats.paroleChiave.splice(this.gameStats.paroleChiave.indexOf(item),1);
+            this.activeEdit = null; 
+        },
+        removeNota: function(item){
+            this.gameStats.note.splice(this.gameStats.note.indexOf(item),1);
+            this.activeEdit = null; 
+        },
+        markSangue: function(event)
+        {
+            targetId = event.currentTarget.id;
+            $('#'+targetId).toggleClass('btn-outline-light');
+            $('#'+targetId).toggleClass('btn-danger');
+
+            $('.box-sangue').removeClass('box-sangue-0');
+            $('.box-sangue').removeClass('box-sangue-1');
+            $('.box-sangue').removeClass('box-sangue-2');
+            $('.box-sangue').removeClass('box-sangue-3');
+            $('.box-sangue').removeClass('box-sangue-4');
+            $('.box-sangue').addClass('box-sangue-' + $('.box-sangue > .btn-danger').length);
+        }
+    },
+    beforeCreate: function(){
+        console.log("beforeCreate: " + ((new Date()).getTime() - statTime).toString());
+    },
+    created: function(){
+        console.log("created: " + ((new Date()).getTime() - statTime).toString());
+        this.loadFromLocalDB();
+        console.log("end created: " + ((new Date()).getTime() - statTime).toString());
+    },
+    mounted: function(){
+        console.log("mounted: " + ((new Date()).getTime() - statTime).toString());
+        //after all loaded
+        this.$nextTick(function() {
+            console.log("nextTick: " + ((new Date()).getTime() - statTime).toString());
+            console.log("end nextTick: " + ((new Date()).getTime() - statTime).toString());
+        });
+    }
+});
+
+Vue.directive('visible', (el, bind) => {
+    el.style.visibility=(!!bind.value) ? 'visible' : 'hidden';});
+
+var footer = new Vue({
+    i18n,
+    el: '#footer'
+});
+
+var a;
